1993c63a31918e95541b7246531f02ec9b0501ef
milagorecki <mila.gorecki@student.uni-tuebingen.de>
2022-05-18 08:34:32 +0200

    add checkpoints

--- loss_calibration/classifier.py
+++ loss_calibration/classifier.py
@@ -1,9 +1,15 @@
 import torch
 from torch import nn
+import shutil
+from os import path
+from typing import Callable
+from loss_calibration.loss import BCELoss_weighted
 
 
 class FeedforwardNN(nn.Module):
-    def __init__(self, input_dim, hidden_dims, output_dim):
+    def __init__(
+        self, input_dim: int = 1, hidden_dims: list = [5], output_dim: int = 1
+    ):
         """initialize the neural network
 
         Args:
@@ -32,7 +38,7 @@ class FeedforwardNN(nn.Module):
             epochs=[], weights=[], treshold=[], optimizer=[], ntrain=[]
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         out = self.input_layer(x)
         out = self.sigmoid(out)
 
@@ -46,13 +52,13 @@ class FeedforwardNN(nn.Module):
 
     def training(
         self,
-        x_train,
-        th_train,
-        threshold,
-        epochs,
-        criterion,
+        x_train: torch.Tensor,
+        th_train: torch.Tensor,
+        threshold: float,
+        epochs: int,
+        criterion: Callable,
         optimizer,
-        batch_size=10000,
+        batch_size: int = 10000,
     ):
         d_train = (th_train > threshold).float()
         # dataset = data.TensorDataset(th_train, x_train, d_train)
@@ -81,7 +87,7 @@ class FeedforwardNN(nn.Module):
                 )
         self._summary = dict(
             epochs=epochs,
-            weights=None,
+            costs=None,
             treshold=threshold,
             optimizer=optimizer,
             ntrain=th_train.shape[0],
@@ -92,7 +98,7 @@ class simpleNN(nn.Module):
 
     # /!\ limited to 1 hidden layer
 
-    def __init__(self, input_dim, hidden_dim, output_dim):
+    def __init__(self, input_dim: int = 1, hidden_dim: int = 5, output_dim: int = 1):
         super(simpleNN, self).__init__()
         # Linear function
         self.fc1 = nn.Linear(input_dim, hidden_dim)
@@ -101,7 +107,7 @@ class simpleNN(nn.Module):
         # Activation function
         self.sigmoid = nn.Sigmoid()
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         out = self.fc1(x)
         out = self.sigmoid(out)
         out = self.fc2(out)
@@ -110,36 +116,84 @@ class simpleNN(nn.Module):
 
 
 def train(
-    model, x_train, th_train, threshold, epochs, criterion, optimizer, batch_size=10000
+    model: nn.Module,
+    x_train: torch.Tensor,
+    th_train: torch.Tensor,
+    costs: list,
+    threshold: float,
+    stop_after_epochs: int,
+    max_num_epochs: int,
+    learning_rate: float = 5e-4,
+    batch_size: int = 10000,
+    resume_training: bool = False,
+    ckp_path: str = None,
+    model_dir: str = None,
 ):
+    """train classifier until convergence
+
+    Args:
+        model (nn.Module): classifier to train
+        x_train (torch.Tensor): training data - observations
+        th_train (troch.Tensor): training data - parameters
+        threshold (float): threshold for binary decisions
+        stop_after_epochs (int): number of epoch to wait for improvement on the validation data before terminating training
+        max_num_epochs (int): maximum number of epoch to train
+        criterion (Callable): Loss criterion to compute the incurred loss given prediction, true label and theta
+        optimizer (_type_): Optimizer
+        batch_size (int, optional): Training batch size. Defaults to 10000.
+        resume_training (bool, optional): Whether to resume training. Defaults to True.
+
+    Returns:
+        Tuple[nn.Module, torch.Tensor]: trained classifier, loss values
+    """
+    max_num_epochs = 2**31 - 1 if max_num_epochs is None else max_num_epochs
+
     d_train = (th_train > threshold).float()
     # dataset = data.TensorDataset(th_train, x_train, d_train)
     # train_loader = data.DataLoader(dataset, batch_size=batch_size)  # , shuffle=True)
 
-    model.train()
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    criterion = BCELoss_weighted(costs, threshold)
 
-    loss_values = []
-    for epoch in range(epochs):
+    start_epoch = 0
+    loss_values = torch.empty([0])
+    if resume_training:
+        model, optimizer, start_epoch, loss_values = load_checkpoint(
+            ckp_path, model, optimizer
+        )
+
+    for epoch in range(start_epoch, max_num_epochs):
         # for theta_batch, x_batch, d_batch in train_loader:
+        model.train()
         optimizer.zero_grad()
 
         predictions = model(x_train)
-        loss = criterion(predictions, d_train, th_train)  # ! requires gt theta
+        loss = criterion(predictions, d_train, th_train).mean(dim=0)
 
         with torch.no_grad():
-            loss_values.append(loss.item())
+            loss_values = torch.cat((loss_values, loss))
 
         loss.backward()
         optimizer.step()
 
-        if epoch % 100 == 0:
+        if (epoch + 1) % 200 == 0:  # TODO: or model is better
+            checkpoint = {
+                "epoch": epoch + 1,
+                "state_dict": model.state_dict(),
+                "optimizer": optimizer.state_dict(),
+                "training_loss": loss_values,
+            }
+            is_best = False  # TODO: validation performance
+
+            save_checkpoint(checkpoint, is_best, model_dir)
+
             print(
-                f"{epoch}\t c = {predictions.mean():.8f}\t L = {loss.item():.8f}",
+                f"{epoch}\t L = {loss.item():.8f}",
                 end="\r",
             )
 
     model._summary = dict(
-        epochs=epochs,
+        epochs=max_num_epochs,
         weights=None,
         treshold=threshold,
         optimizer=optimizer,
@@ -147,3 +201,18 @@ def train(
     )
 
     return model, loss_values
+
+
+def save_checkpoint(state, is_best, model_dir):
+    f_path = path.join(model_dir, f"checkpoints/checkpoint_e{state['epoch']}.pt")
+    torch.save(state, f_path)
+    if is_best:
+        best_fpath = path.join(model_dir, "best_model.pt")
+        shutil.copyfile(f_path, best_fpath)
+
+
+def load_checkpoint(checkpoint_path, model, optimizer):
+    checkpoint = torch.load(checkpoint_path)
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+    return model, optimizer, checkpoint["epoch"] - 1, checkpoint["training_loss"]
