707ee641db0996399307b21131dd8dc783a4edcf
milagorecki <mila.gorecki@student.uni-tuebingen.de>
2022-05-18 08:35:54 +0200

    add checkpoints

--- scripts/train_classifier.py
+++ scripts/train_classifier.py
@@ -2,20 +2,18 @@
 
 import argparse
 from loss_calibration.classifier import FeedforwardNN, train
-from loss_calibration.loss import BCELoss_weighted
 import torch
 from datetime import datetime
-from os import path
+from os import path, mkdir
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import csv
+import json
 
 
 def main(args):
-    print(
-        f"Training specification:\nepochs: {args.epochs}\nweights: {args.weights}\nthreshold: {args.T}\
-            \nntrain: {args.ntrain}\ndata_dir: {args.data_dir}\nsave_dir: {args.save_dir}\n"
-    )
+    torch.manual_seed(args.seed)
+
     # load data
     th_train = torch.load(path.join(args.data_dir, "th_train.pt"))
     x_train = torch.load(path.join(args.data_dir, "x_train.pt"))
@@ -26,45 +24,78 @@ def main(args):
         x_train = x_train[: args.ntrain]
 
     threshold = args.T
-    weights = args.weights
+    costs = args.costs
+
+    # create directory & save metadata
+    timestamp = datetime.now().isoformat().split(".")[0].replace(":", "_")
+    model_dir = path.join(
+        args.save_dir, f"{timestamp}_t{int(threshold)}_c{int(costs[0])}_{int(costs[1])}"
+    )
+    try:
+        mkdir(model_dir)
+        mkdir(path.join(model_dir, "checkpoints/"))
+        print(f"Directory {model_dir} created.")
+    except FileExistsError:
+        print(f"Directory {model_dir} already exists.")
+    metadata = {
+        "seed": args.seed,
+        "architecture": "1-16-1",  # TODO: save correct model architecture
+        "optimizer": "Adam",
+        "Ntrain": th_train.shape[0],
+        "threshold": threshold,
+        "costs": costs,
+    }
+    json.dump(metadata, open(f"{model_dir}/metadata.json", "w"))
 
     # training
+    print(
+        f"Training specification:\nseed: {args.seed}\nepochs: {args.epochs}\nweights: {args.costs}\nthreshold: {args.T}\
+            \nntrain: {args.ntrain}\ndata_dir: {args.data_dir}\nsave_dir: {args.save_dir}\n"
+    )
     clf = FeedforwardNN(1, [16], 1)
 
     epochs = args.epochs
-    optimizer = torch.optim.Adam(clf.parameters())
-    criterion = BCELoss_weighted(weights, threshold)
 
     clf, loss_values = train(
-        clf, x_train, th_train, threshold, epochs, criterion, optimizer
+        clf,
+        x_train,
+        th_train,
+        costs,
+        threshold,
+        stop_after_epochs=0,
+        max_num_epochs=epochs,
+        model_dir=model_dir,
     )
-    clf._summary["weights"] = weights  # work-around to save weights
+    clf._summary["costs"] = costs  # work-around to save weights
 
     # save trained classifier and metadata
     timestamp = datetime.now().isoformat().split(".")[0].replace(":", "_")
-    torch.save(clf, path.join(args.save_dir, f"{timestamp}_1d_classifier.pt"))
-    torch.save(clf, path.join(args.save_dir, f"{timestamp}_loss.pt"))
+    torch.save(clf, path.join(model_dir, f"{timestamp}_final_model.pt"))
+    torch.save(loss_values, path.join(model_dir, f"{timestamp}_loss.pt"))
     with open(
         path.join(args.save_dir, "classifiers.csv"), "a", encoding="UTF8", newline=""
     ) as f:
         writer = csv.writer(f)
         writer.writerow(
-            [timestamp, str(weights), threshold, epochs, optimizer, th_train.shape[0]]
+            [timestamp, str(costs), threshold, epochs, "Adam", th_train.shape[0]]
         )
 
     plt.plot(torch.arange(epochs).detach().numpy(), loss_values)
     plt.title("Loss curve")
     plt.ylabel("loss")
     plt.xlabel("epochs")
-    plt.savefig(path.join(args.save_dir, f"{timestamp}_loss.pdf"))
+    plt.savefig(path.join(model_dir, f"{timestamp}_loss.pdf"))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Training the classifier")
 
+    parser.add_argument(
+        "--seed", type=int, default=9834, help="Set seed for reproducibility."
+    )
     parser.add_argument("--epochs", type=int, default=5000, help="Number of epochs")
     parser.add_argument(
-        "--weights",
+        "--costs",
         type=lambda s: [float(item) for item in s.split(",")],
         default=[1.0, 1.0],
         help="List specifying the cost of misclassification",
