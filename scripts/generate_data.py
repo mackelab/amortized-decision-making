e60de9821eee5c2508eb0dc0477278603b099040
milagorecki <mila.gorecki@student.uni-tuebingen.de>
2022-05-26 10:54:22 +0200

    generate validation data

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -19,9 +19,10 @@ def main(args):
         )  # * (theta+3)
 
     n_train = args.ntrain
+    n_val = args.nval
     n_test = args.ntest
 
-    thetas = prior.sample((n_train + n_test,))
+    thetas = prior.sample((n_train + n_test + n_val,))
     observations = simulator(thetas)
 
     # threshold = args.T
@@ -30,8 +31,12 @@ def main(args):
     # save data
     torch.save(thetas[:n_train], path.join(args.data_dir, "th_train.pt"))
     torch.save(observations[:n_train], path.join(args.data_dir, "x_train.pt"))
-    torch.save(thetas[n_train:], path.join(args.data_dir, "th_test.pt"))
-    torch.save(observations[n_train:], path.join(args.data_dir, "x_test.pt"))
+    torch.save(thetas[n_train : n_train + n_val], path.join(args.data_dir, "th_val.pt"))
+    torch.save(
+        observations[n_train : n_train + n_val], path.join(args.data_dir, "x_val.pt")
+    )
+    torch.save(thetas[n_train + n_val :], path.join(args.data_dir, "th_test.pt"))
+    torch.save(observations[n_train + n_val :], path.join(args.data_dir, "x_test.pt"))
 
     print(f"Saved data to '{args.data_dir}'.")
 
@@ -40,13 +45,18 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a new data set")
 
     parser.add_argument(
-        "--ntrain", type=int, default=50000, help="Number of training samples"
+        "--ntrain", type=int, default=500000, help="Number of training samples"
     )
     parser.add_argument(
-        "--ntest", type=int, default=5000, help="Number of validation samples"
+        "--nval", type=int, default=100000, help="Number of validation samples"
     )
     parser.add_argument(
-        "--data_dir", required=True, help="Directory to save the data set"
+        "--ntest", type=int, default=100000, help="Number of test samples"
+    )
+    parser.add_argument(
+        "--data_dir",
+        default="../data/1d_classifier",
+        help="Directory to save the data set",
     )
 
     args = parser.parse_args()
