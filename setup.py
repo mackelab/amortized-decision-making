d5f7bdc0a40e100cf0cf281aebcf2ea857bfe46a
milagorecki <mila.gorecki@student.uni-tuebingen.de>
2022-06-23 16:53:52 +0200

    update required

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 from setuptools import find_packages, setup
 
+# Package meta-data.
+NAME = "loss-calibration"
+URL = "https://github.com/mackelab/loss-calibration"
+EMAIL = "mila.gorecki@student.uni-tuebingen.de"
+AUTHOR = "Mila Gorecki"
+REQUIRES_PYTHON = ">=3.6.0"
+
+REQUIRED = ["torch", "numpy", "matplotlib", "sbi", "sbibm"]
+
 setup(
-    name='loss-calibration',
+    name=NAME,
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
     packages=find_packages(),
+    install_requires=REQUIRED,
 )
