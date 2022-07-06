1e31fb2bb5c66ae3cd357c2b6cf7cbd825520c41
milagorecki <mila.gorecki@student.uni-tuebingen.de>
2022-07-06 14:36:10 +0200

    vectorize + add sigmoidal loss function

--- loss_calibration/loss.py
+++ loss_calibration/loss.py
@@ -14,25 +14,84 @@ def StepLoss_weighted(weights, threshold):
 
     assert len(weights) == 2, f"Expected 2, got {len(weights)}"
 
-    def loss(theta, decision):
+    def loss(true_theta, decision, dim=None):
         """custom loss function (BCE with class weights)
 
         Args:
-            theta (torch.Tensor): observed/true parameter value
-            decision (int): indicates decision: 0 (below threshold) or  1(above treshold)
-            threshold (float, optional): threshold for binarized decisons, defaults to 5.0.
+            theta (torch.Tensor): observed/true parameter values
+            decision (torch.Tensor or float): indicates decision: 0 (below threshold) or  1(above treshold)
+            threshold (torch.Tensor): threshold for binarized decisons.
 
         Returns:
             float: incurred loss
         """
-        assert decision in [
-            0,
-            1,
-        ], "decision has to be one of 0 (below threshold) or  1(above treshold)"
+
+        if type(decision) == float or type(decision) == int:
+            assert (
+                decision == 0.0 or decision == 1.0
+            ), "Decision has to be either 0 or 1"
+        else:
+            assert torch.logical_or(
+                decision == 0.0, decision == 1.0
+            ).all(), (
+                "All values have to be either 0 (below threshold) or  1(above treshold)"
+            )
 
         return (
-            decision * (1 - torch.gt(theta, threshold).type(torch.float)) * weights[1]
-            + (1 - decision) * torch.gt(theta, threshold).type(torch.float) * weights[0]
+            decision
+            * (1 - torch.gt(true_theta, threshold).type(torch.float))
+            * weights[1]
+            + (1 - decision)
+            * torch.gt(true_theta, threshold).type(torch.float)
+            * weights[0]
+        )
+
+    return loss
+
+
+def SigmoidLoss_weighted(weights, threshold):
+    """Sigmoid Loss, differentiable approximation of Step Loss
+
+    Args:
+        weights (list): cost of misclassification, weights[0] = cost of FN, weights[1]=cost of FP
+        threshold (float): threshold for decision-making
+
+    Returns:
+        function: loss function L(theta, prediction)
+    """
+
+    assert len(weights) == 2, f"Expected 2, got {len(weights)}"
+
+    def loss(true_theta, decision, dim=None, slope=100):
+        """custom loss function (BCE with class weights)
+
+        Args:
+            theta (torch.Tensor): observed/true parameter values
+            decision (torch.Tensor or float): indicates decision: 0 (below threshold) or  1(above treshold)
+            threshold (torch.Tensor): threshold for binarized decisons.
+
+        Returns:
+            float: incurred loss
+        """
+
+        if type(decision) == float or type(decision) == int:
+            assert (
+                decision == 0.0 or decision == 1.0
+            ), "Decision has to be either 0 or 1"
+        else:
+            assert torch.logical_or(
+                decision == 0.0, decision == 1.0
+            ).all(), (
+                "All values have to be either 0 (below threshold) or  1(above treshold)"
+            )
+
+        return (
+            decision
+            * (1 - torch.sigmoid(slope * (true_theta - threshold)))
+            * weights[1]
+            + (1 - decision)
+            * torch.sigmoid(slope * (true_theta - threshold))
+            * weights[0]
         )
 
     return loss
@@ -51,7 +110,7 @@ def LinearLoss_weighted(weights, threshold):
 
     assert len(weights) == 2, f"Expected 2, got {len(weights)}"
 
-    def loss(theta, decision):
+    def loss(true_theta, decision):
         """custom loss function (BCE with class weights)
 
         Args:
@@ -62,19 +121,30 @@ def LinearLoss_weighted(weights, threshold):
         Returns:
             float: incurred loss
         """
-        assert decision in [
-            0,
-            1,
-        ], "decision has to be one of 0 (below threshold) or  1(above treshold)"
+        if type(decision) == float or type(decision) == int:
+            assert (
+                decision == 0.0 or decision == 1.0
+            ), "Decision has to be either 0 or 1"
+        else:
+            assert (
+                true_theta.shape == decision.shape
+            ), f"Shapes must match, got {true_theta.shape} and {decision.shape}."
+            assert torch.logical_or(
+                decision == 0.0, decision == 1.0
+            ).all(), (
+                "All values have to be either 0 (below threshold) or  1(above treshold)"
+            )
 
-        return decision * (1 - torch.gt(theta, threshold).type(torch.float)) * weights[
-            1
-        ] * (threshold - theta) + (1 - decision) * torch.gt(theta, threshold).type(
+        return decision * (
+            1 - torch.gt(true_theta, threshold).type(torch.float)
+        ) * weights[1] * (threshold - true_theta) + (1 - decision) * torch.gt(
+            true_theta, threshold
+        ).type(
             torch.float
         ) * weights[
             0
         ] * (
-            theta - threshold
+            true_theta - threshold
         )
 
     return loss
@@ -92,7 +162,7 @@ def LinearLoss_weighted(weights, threshold):
 # if w0 >> w1 --> avoid FN, okay with FP
 
 
-def BCELoss_weighted(weights, threshold, cost_func="step"):
+def BCELoss_weighted(weights, threshold, cost_fn="step"):
     def loss(prediction, target, theta):
         """computation of incurred costs
 
@@ -107,7 +177,7 @@ def BCELoss_weighted(weights, threshold, cost_func="step"):
         assert (
             prediction.shape == target.shape == theta.shape
         ), "All arguments should have the same shape."
-        assert cost_func in [
+        assert cost_fn in [
             "step",
             "linear",
         ], "The cost function has to be one of 'step' or 'linear'."
@@ -116,11 +186,12 @@ def BCELoss_weighted(weights, threshold, cost_func="step"):
             "step": StepLoss_weighted(weights, threshold),
             "linear": LinearLoss_weighted(weights, threshold),
         }
-        costs = cost_functions[cost_func]
+        costs = cost_functions[cost_fn]
+
         prediction = torch.clamp(prediction, min=1e-7, max=1 - 1e-7)
-        bce = -target * torch.log(prediction) * costs(theta, 0) - (
+        bce = -target * torch.log(prediction) * costs(theta, 0.0) - (
             1 - target
-        ) * torch.log(1 - prediction) * costs(theta, 1)
+        ) * torch.log(1 - prediction) * costs(theta, 1.0)
         return bce
 
     return loss
