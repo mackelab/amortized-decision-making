565852e5b7c37eee4a9ec75629527e1ae2ecdaf0
milagorecki <mila.gorecki@student.uni-tuebingen.de>
2022-08-08 13:01:54 +0200

    add parameter again

--- scripts/compute_npe_predictions.py
+++ scripts/compute_npe_predictions.py
@@ -63,7 +63,7 @@ def main(cfg: DictConfig):
             path.join(
                 cfg.res_dir,
                 task_name,
-                f"npe/{cfg.experiment}/{estimator}_n{nsim}_predictions_t{threshold}_c{int(costs[0])}_{int(costs[1])}.pt",
+                f"npe/{cfg.experiment}/{estimator}_n{nsim}_predictions_t{parameter}_{threshold}_c{int(costs[0])}_{int(costs[1])}.pt",
             ),
         )
     elif task_name == "toy_example":
